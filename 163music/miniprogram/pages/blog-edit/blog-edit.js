beb62a48818f86801396465a3b8e94fb0fd72d82
zch1999 <1901394767@qq.com>
2020-01-30 23:01:15 +0800

    发布博客功能

--- 163music/miniprogram/pages/blog-edit/blog-edit.js
+++ 163music/miniprogram/pages/blog-edit/blog-edit.js
@@ -2,6 +2,11 @@
 const MAX_WORDS_NUM = 140
 //最大上传图片数量
 const MAX_IMG_NUM = 9
+//内容
+let content = ''
+//个人信息
+let userInfo = {}
+const db = wx.cloud.database()
 Page({
 
   /**
@@ -23,6 +28,7 @@ Page({
     this.setData({
       wordsNum
     })
+    content = event.detail.value
   },
 
 //得到焦点
@@ -61,11 +67,102 @@ Page({
       },
     })
   },
+
+//删减图片
+  onDelImage(event){
+    this.data.images.splice(event.target.dataset.index, 1)
+    this.setData({
+      images: this.data.images
+    })
+    if (this.data.images.length == MAX_IMG_NUM - 1){
+      this.setData({
+        selectPhoto: true
+      })
+    }
+  },
+
+//预览图片
+  onPreviewImage(event){
+    wx.previewImage({
+      urls: this.data.images,
+      current:event.target.imgsrc
+    })
+  },
+
+  send(){
+    //2，数据 -> 云数据库
+    //数据库：内容，图片fileID,openid,昵称，头像，时间
+    //1，图片 -> 云存储 返回fileID 云文件id
+
+    if(content.trim() === ''){
+      wx.showModal({
+        title: '请输入内容',
+        content: '',
+      })
+      return
+    }
+
+    wx.showLoading({
+      title: '发布中',
+    })
+
+    let promiseArr = []
+    let fileIds = []
+    //图片上传
+    for(let i = 0, len = this.data.images.length; i< len; i++){
+      let p = new Promise((resolve,reject)=>{
+        let item = this.data.images[i]
+        let suffix = /\.\w+$/.exec(item)[0]
+        wx.cloud.uploadFile({
+          cloudPath: 'blog/' + Date.now() + '-' + Math.random() * 10000000 + suffix,
+          filePath: item,
+          success: (res) => {
+            console.log(res)
+            fileIds = fileIds.concat(res.fileID)
+            resolve()
+          },
+          fail: (err) => {
+            console.log(err)
+            reject()
+          }
+        })
+      })
+      promiseArr.push(p)
+    }
+    //存入云数据库
+    Promise.all(promiseArr).then((res)=>{
+      db.collection('blog').add({
+        data:{
+          ...userInfo,
+          content,
+          img: fileIds,
+          createTime: db.serverDate() //服务端时间
+        }
+      }).then((res)=>{
+        wx.hideLoading()
+        wx.showToast({
+          title: '发布成功',
+        })
+        
+        //返回blog页面，并且刷新
+        wx.navigateBack({
+          
+        })
+      }).catch((err)=>{
+        wx.hideLoading()
+        wx.showToast({
+          title: '发布失败',
+        })
+      })
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    // console.log(options)
+    userInfo = options
   },
 
   /**
